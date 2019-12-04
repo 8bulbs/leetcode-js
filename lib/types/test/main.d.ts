e5e4d644104532b2daf176fabbbc4aca9253ade8
liaoxiaomin <liaoxiaomin@vipkid.com.cn>
2019-12-04 11:33:59 +0800

    my linked list

--- lib/types/test/main.d.ts
+++ lib/types/test/main.d.ts
@@ -1,1 +1,22 @@
-export {};
+declare let operationCount: number;
+declare function defineReactive(data: any, key: any, value: any): void;
+declare function observe(data: any): void;
+declare const array: any[];
+declare function logOperationCount(caseName: any, cb: any, ...args: any[]): void;
+declare class LinkedListNode {
+    data: any;
+    next: LinkedListNode;
+    constructor(data: any);
+}
+declare function checkRange(): Function;
+declare function logMethod(target: Object, propertyName: string, propertyDescriptor: PropertyDescriptor): PropertyDescriptor;
+declare class LinkedList {
+    private head;
+    private tail;
+    private size;
+    insert(data: any, index?: number): void;
+    get(index: number): LinkedListNode;
+    remove(index: number): LinkedListNode;
+    print(): void;
+}
+declare const myList: LinkedList;
