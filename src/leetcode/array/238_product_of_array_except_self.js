0f7a7dc28dc9025918f5cac6fa2eea42698c34cc
liaoxiaomin <liaoxiaomin@vipkid.com.cn>
2019-12-24 00:28:26 +0800

    238_product_of_array_except_self

---
+++ src/leetcode/array/238_product_of_array_except_self.js
@@ -0,0 +1,30 @@
+
+// Input:  [1,2,3,4]
+// Output: [24,12,8,6]
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+  let res = []
+  let leftTotal = 1
+  let leftArr = []
+  let rightTotal = 1
+  let rightArr = []
+  const length = nums.length
+  for (let i = 0; i < length; ++i) {
+    leftArr.push(leftTotal)
+    leftTotal *= nums[i]
+    rightArr[length - i - 1] = rightTotal
+    rightTotal *= nums[length - i - 1]
+  }
+  for (let j = 0; j < length; ++j) {
+    res.push(leftArr[j] * rightArr[j])
+  }
+  // console.warn('leftArr', leftArr)
+  // console.warn('rightArr', rightArr)
+  // console.warn('res', res)
+  return res
+};
+productExceptSelf([1,2,3,4])
