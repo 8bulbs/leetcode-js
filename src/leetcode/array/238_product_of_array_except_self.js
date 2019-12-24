571fa531c2cd6edf0a625a39e1e7f17cb9ca7982
liaoxiaomin <liaoxiaomin@vipkid.com.cn>
2019-12-24 10:33:12 +0800

    59_spiral_matrix_ii

--- src/leetcode/array/238_product_of_array_except_self.js
+++ src/leetcode/array/238_product_of_array_except_self.js
@@ -27,4 +27,21 @@ var productExceptSelf = function(nums) {
   // console.warn('res', res)
   return res
 };
+
+var productExceptSelf = function(nums) {
+  const length = nums.length
+  let res = new Array(length).fill(1)
+  let leftTotal = 1
+  let rightTotal = 1
+  for (let i = 0; i < length; ++i) {
+    res[i] = leftTotal
+    leftTotal *= nums[i]
+    res[length - i - 1] *= rightTotal
+    rightTotal *= nums[length - i - 1]
+  }
+  // console.warn('leftArr', leftArr)
+  // console.warn('rightArr', rightArr)
+  console.warn('res', res)
+  return res
+};
 productExceptSelf([1,2,3,4])
