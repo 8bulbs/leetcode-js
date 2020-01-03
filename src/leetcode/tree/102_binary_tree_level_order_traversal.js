5a17cb8006d57f26cf97d182ab0a6b310437ffb6
liaoxiaomin <liaoxiaomin@vipkid.com.cn>
2020-01-03 16:53:22 +0800

    102_binary_tree_level_order_traversal

---
+++ src/leetcode/tree/102_binary_tree_level_order_traversal.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+  const res = []
+  function travelsal (root, depth) {
+    if (root !== null) {
+      if (!res[depth]) {
+        res[depth] = []
+      }
+      travelsal(root.left, depth + 1)
+      res[depth].push(root.val)
+      travelsal(root.right, depth + 1)
+    }
+  }
+  travelsal(root, 0)
+  return res
+};
