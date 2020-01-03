4818dcfbd2edb1917211693ab6919759a71d5ca9
liaoxiaomin <liaoxiaomin@vipkid.com.cn>
2020-01-03 17:28:21 +0800

    145_binary_tree_postorder_traversal

--- src/leetcode/tree/145_binary_tree_postorder_traversal.js
+++ src/leetcode/tree/145_binary_tree_postorder_traversal.js
@@ -11,5 +11,13 @@
  */
 var postorderTraversal = function(root) {
   const res = []
-  
+  function traversal (root) {
+    if (root !== null) {
+      traversal(root.left)
+      traversal(root.right)
+      res.push(root.val)
+    }
+  }
+  traversal(root)
+  return res
 };
