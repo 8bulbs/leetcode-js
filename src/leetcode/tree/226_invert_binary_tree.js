b2fecc7a6d9742d47f3073471fd78c01c3f651ca
liaoxiaomin <liaoxiaomin@vipkid.com.cn>
2020-01-15 10:36:31 +0800

    226_invert_binary_tree

--- src/leetcode/tree/226_invert_binary_tree.js
+++ src/leetcode/tree/226_invert_binary_tree.js
@@ -19,4 +19,17 @@ var invertTree = function(root) {
     invertTree(root.left)
     invertTree(root.right)
   }
+  return root
 };
+
+var invertTree = function(root) {
+  function traversal (root) {
+    if (root === null) {
+      return null
+    } else {
+      [root.left, root.right] = [traversal(root.right), traversal(root.left)]
+      return root
+    }
+  }
+  return  traversal(root)
+}
