a68add5b931894e5ad7126f5f8b1e44d370bc760
liaoxiaomin <liaoxiaomin@vipkid.com.cn>
2020-01-17 17:28:50 +0800

    opt 103

--- src/leetcode/tree/103_binary_tree_zigzag_level_order_traversal.js
+++ src/leetcode/tree/103_binary_tree_zigzag_level_order_traversal.js
@@ -33,3 +33,27 @@ var zigzagLevelOrder = function(root) {
     return res
   }
 };
+
+var zigzagLevelOrder = function(root) {
+  if (root === null) {
+    return []
+  } else {
+    let res = []
+    function traversal (root, depth) {
+      if (root !== null) {
+        if (res[depth] === undefined) {
+          res[depth] = []
+        }
+        if (depth & 1) {
+          res[depth].unshift(root.val)
+        } else {
+          res[depth].push(root.val)
+        }
+        traversal(root.left, depth + 1)
+        traversal(root.right, depth + 1)
+      }
+    }
+    traversal(root, 0)
+    return res
+  }
+};
