3dd40ac8ececf08789b87a3833e15dc1c7e0e7ba
liaoxiaomin <liaoxiaomin@vipkid.com.cn>
2020-01-30 11:44:28 +0800

    209_minimum_size_subarray_sum

--- src/leetcode/sliding_window/76_minimum_window_substring.js
+++ src/leetcode/sliding_window/76_minimum_window_substring.js
@@ -9,10 +9,10 @@ var minWindow = function(s, t) {
   } else {
     let leftPointer = 0
     let rightPointer = 0
-    let windowHashTable = {}
     let res = s
     let existFlag = false
     const targetHashTable = {}
+    const windowHashTable = {}
     // 生成目标哈希表
     for (let i = t.length - 1; i >= 0; i--) {
       if (targetHashTable[t[i]] === undefined) {
@@ -21,6 +21,7 @@ var minWindow = function(s, t) {
         targetHashTable[t[i]]++
       }
     }
+    // 是否符合要求
     function isFit (windowHashTable, targetHashTable) {
       for (let str in targetHashTable) {
         if (windowHashTable[str] === undefined) {
@@ -35,6 +36,7 @@ var minWindow = function(s, t) {
       return true
     }
     while (rightPointer < s.length) {
+      // 向右移动右指针, 并在窗口哈希表记录每个字符出现的次数
       const currentStr = s[rightPointer]
       if (windowHashTable[currentStr] === undefined) {
         windowHashTable[currentStr] = 1
@@ -42,7 +44,9 @@ var minWindow = function(s, t) {
         windowHashTable[currentStr]++
       }
       rightPointer++
+      // 向右移动左指针, 缩小窗口, 直至不符合要求
       while (isFit(windowHashTable, targetHashTable)) {
+        // 更新结果
         const _res = s.substring(leftPointer, rightPointer)
         if (_res.length < res.length) {
           res = _res
