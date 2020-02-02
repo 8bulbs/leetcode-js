16cef0c1930255813628d45dbe22e4d15ec867d7
liaoxiaomin <liaoxiaomin@vipkid.com.cn>
2020-02-02 11:37:24 +0800

    234_palindrome_linked_list

--- src/leetcode/linked_list/234_palindrome_linked_list.js
+++ src/leetcode/linked_list/234_palindrome_linked_list.js
@@ -10,26 +10,33 @@
  * @return {boolean}
  */
 var isPalindrome = function(head) {
+  // 考虑空链表
   if (head === null) {
     return true
-  }
-  let headPointer = head
-  let tailPointer = head
-  while (tailPointer.next) {
-    tailPointer.next.prev = tailPointer
-    tailPointer = tailPointer.next
-  }
-  while(headPointer !== tailPointer) {
-    if (headPointer.next !== tailPointer) {
-      if (headPointer.val === tailPointer.val) {
-        headPointer = headPointer.next
-        tailPointer = tailPointer.prev
-      } else {
-        return false
-      }
-    } else {
-      return headPointer.val === tailPointer.val
+  } else {
+    // 头指针
+    let headPointer = head
+    // 尾指针
+    let tailPointer = head
+    // 把链表变成双向链表
+    while (tailPointer.next) {
+      tailPointer.next.prev = tailPointer
+      tailPointer = tailPointer.next
     }
+    // 分别从头尾向中间依次比较
+    while(headPointer !== tailPointer) {
+      if (headPointer.next !== tailPointer) {
+        if (headPointer.val === tailPointer.val) {
+          headPointer = headPointer.next
+          tailPointer = tailPointer.prev
+        } else {
+          return false
+        }
+      // 考虑偶数链表
+      } else {
+        return headPointer.val === tailPointer.val
+      }
+    }
+    return true
   }
-  return true
 };
