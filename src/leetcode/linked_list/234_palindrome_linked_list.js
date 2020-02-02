d348aba39f77c212c21dbc57b7df3c91e235aa43
liaoxiaomin <liaoxiaomin@vipkid.com.cn>
2020-02-02 23:12:25 +0800

    234_palindrome_linked_list

--- src/leetcode/linked_list/234_palindrome_linked_list.js
+++ src/leetcode/linked_list/234_palindrome_linked_list.js
@@ -40,3 +40,48 @@ var isPalindrome = function(head) {
     return true
   }
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+  if (head === null) {
+    return true
+  } else if (head.next === null) {
+    return true
+  } else {
+    let slowPointer = head
+    let fastPointer = head
+    let _head = null
+    let temp = null
+    // 找到中间节点, 并翻转前半截链表,
+    // 让_head指向翻转后的前半截链表的头部,
+    // 让slow指向后半截链表的头节点
+    while (fastPointer && fastPointer.next) {
+      _head = slowPointer
+      slowPointer = slowPointer.next
+      fastPointer = fastPointer.next.next
+      _head.next = temp
+      temp = _head
+    }
+    if (fastPointer) {
+      slowPointer = slowPointer.next
+    }
+    while (_head) {
+      if (_head.val !== slowPointer.val) {
+        return false
+      }
+      _head = _head.next
+      slowPointer = slowPointer.next
+    }
+    return true
+  }
+};
