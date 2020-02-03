7e0d4a1e139d90af39f60a2e2d9aaabee736eff4
liaoxiaomin <liaoxiaomin@vipkid.com.cn>
2020-02-03 10:28:43 +0800

    9_palindrome_number

--- src/leetcode/mathematics/9_palindrome_number.js
+++ src/leetcode/mathematics/9_palindrome_number.js
@@ -35,4 +35,27 @@ var isPalindrome = function(x) {
   }
 };
 
+
+var isPalindrome = function(x) {
+  // 负数
+  if (x < 0) {
+    return false
+  // 0至9
+  } else if (x >= 0 && x < 10) {
+      return true
+  // 末尾数为0的非零数字
+  } else if (x !== 0 && x % 10 === 0) {
+    return false
+  } else {
+      let reverse = 0
+      // 如果翻转数字大于原数字, 则翻转到了中间数位
+      while (x > reverse) {
+          reverse = reverse * 10 + x % 10
+          x = Math.floor(x / 10)
+      }
+      // 考虑奇数位
+      return (reverse === x) || (Math.floor(reverse / 10) === x)
+  }
+};
+
 console.warn(isPalindrome(121))
