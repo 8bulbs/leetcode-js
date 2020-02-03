192bc9c40f1a249b0b57dfa84721fbe5747a3ad0
liaoxiaomin <liaoxiaomin@vipkid.com.cn>
2020-02-03 18:45:18 +0800

    680_valid_palindrome_ii

--- src/leetcode/string/680_valid_palindrome_ii.js
+++ src/leetcode/string/680_valid_palindrome_ii.js
@@ -22,6 +22,37 @@ var validPalindrome = function(s) {
     if (s[headPointer] !== s[tailPointer]) {
       if (s[headPointer + 1] === s[tailPointer] || s[headPointer] === s[tailPointer - 1]) {
         return isPalindrome(s.substring(headPointer + 1, tailPointer + 1)) || isPalindrome(s.substring(headPointer, tailPointer))
+      } else {
+        return false
+      }
+    } else {
+      headPointer++
+      tailPointer--
+    }
+  }
+  return true
+};
+
+var validPalindrome = function(s) {
+  let headPointer = 0
+  let tailPointer = s.length - 1
+  function isPalindrome (s, headPointer, tailPointer) {
+    while (headPointer < tailPointer) {
+      if (s[headPointer] !== s[tailPointer]) {
+        return false
+      } else {
+        headPointer++
+        tailPointer--
+      }
+    }
+    return true
+  }
+  while (headPointer < tailPointer) {
+    if (s[headPointer] !== s[tailPointer]) {
+      if (s[headPointer + 1] === s[tailPointer] || s[headPointer] === s[tailPointer - 1]) {
+        return isPalindrome(s, headPointer + 1, tailPointer) || isPalindrome(s, headPointer, tailPointer - 1)
+      } else {
+        return false
       }
     } else {
       headPointer++
